9cca46bf8d5dc4960daf2504e9bcd3c81ba6f9ca
Rod97139 <fayaflame@gmail.com>
2022-12-29 04:18:06 +0100

    pagination + order by age

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -25,6 +25,18 @@ class PersonneController extends AbstractController
         
     }
 
+    #[Route('/all/{page?1}/{nbre?12}', name: 'personne.list.all')]
+    public function indexAll(ManagerRegistry $doctrine, $page, $nbre): Response
+    {
+        $repository = $doctrine->getRepository(Personne::class);
+        $personnes = $repository->findBy([], ['age' => 'ASC' ], $nbre, ($page - 1) * $nbre );
+
+        return $this->render('personne/index.html.twig', [
+                'personnes' => $personnes,
+            ]);
+        
+    }
+
     #[Route('/{id<\d+>}', name: 'personne.detail')]
     public function detail(Personne $personne = null): Response
     {
