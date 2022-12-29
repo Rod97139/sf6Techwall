1778109f6e99e0d3d0e265917d08a5cfbfc685c3
Rod97139 <fayaflame@gmail.com>
2022-12-29 02:35:52 +0100

    add new personne

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PersonneController extends AbstractController
 {
-    #[Route('/personne', name: 'app_personne')]
-    public function index(): Response
+    #[Route('/personne/add', name: 'personne.add')]
+    public function addPersonne(ManagerRegistry $doctrine): Response
     {
-        return $this->render('personne/index.html.twig', [
-            'controller_name' => 'PersonneController',
+
+        $entityManager = $doctrine-> getManager();
+        
+        $personne = new Personne();
+        $personne->setFirstname('Rod')
+                 ->setName('MINGO')
+                 ->setAge(31);
+
+        $personne2 = new Personne();
+        $personne2->setFirstname('John')
+                 ->setName('Doe')
+                 ->setAge(25);
+
+        //Ajouter l'opération d'insertion de la personne dans ma transaction
+
+        $entityManager->persist($personne);
+        $entityManager->persist($personne2);
+        
+        //Execute la transaction Todo
+
+        $entityManager->flush();
+             return $this->render('personne/detail.html.twig', [
+            'personne' => $personne,
         ]);
     }
 }
